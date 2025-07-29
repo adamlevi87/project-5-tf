053ff3470252f30959cb805858b48d6c13bd8276
Adam Levi <adamlevi87@gmail.com>
2025-07-29 22:16:44 +0300

    reverted back to exec in providers

--- main/providers.tf
+++ main/providers.tf
@@ -45,11 +45,11 @@ provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.cluster.token
-  # exec {
-  #   api_version = "client.authentication.k8s.io/v1beta1"
-  #   command     = "aws"
-  #   args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.cluster_name, "--region", var.aws_region]
-  # }
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.cluster.name, "--region", var.aws_region]
+  }
 }
 
 # Helm provider configuration  
@@ -58,10 +58,10 @@ provider "helm" {
     host                   = data.aws_eks_cluster.cluster.endpoint
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
     token                  = data.aws_eks_cluster_auth.cluster.token
-    # exec {
-    #   api_version = "client.authentication.k8s.io/v1beta1"
-    #   command     = "aws"
-    #   args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name, "--region", var.aws_region]
-    # }
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.cluster.name, "--region", var.aws_region]
+    }
   }
 }
