6a477a42da11cc57340435aab8cc25331fc977cc
Adam Levi <adamlevi87@gmail.com>
2025-07-29 21:49:43 +0300

    updated providers

--- main/providers.tf
+++ main/providers.tf
@@ -34,8 +34,8 @@ data "aws_eks_cluster_auth" "cluster" {
 
 # Kubernetes provider configuration
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.cluster_endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.cluster_certificate_authority_data)
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.cluster.token
   # exec {
   #   api_version = "client.authentication.k8s.io/v1beta1"
@@ -47,8 +47,8 @@ provider "kubernetes" {
 # Helm provider configuration  
 provider "helm" {
   kubernetes {
-    host                   = data.aws_eks_cluster.cluster.cluster_endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.cluster_certificate_authority_data)
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
     token                  = data.aws_eks_cluster_auth.cluster.token
     # exec {
     #   api_version = "client.authentication.k8s.io/v1beta1"
