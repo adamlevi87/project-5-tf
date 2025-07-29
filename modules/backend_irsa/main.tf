1e3a19a30db9ba0c4282476cf2debe83db766072
Adam Levi <adamlevi87@gmail.com>
2025-07-29 14:21:33 +0300

    changed backend irsa

--- modules/backend_irsa/main.tf
+++ modules/backend_irsa/main.tf
@@ -1,16 +1,16 @@
 # modules/backend_irsa/main.tf
 
-locals {
-  # arn:aws:iam::123456789012:oidc-provider/oidc.eks.us-east-1.amazonaws.com/id/ABCDEF1234567890
-  # ["arn:aws:iam::123456789012:", "oidc.eks.us-east-1.amazonaws.com/id/ABCDEF1234567890"]
-  # element[1] means the 2nd part of the variable
-  oidc_provider_host = element(
-    split("oidc-provider/", var.oidc_provider_arn),
-    1
-  )
+# locals {
+#   # arn:aws:iam::123456789012:oidc-provider/oidc.eks.us-east-1.amazonaws.com/id/ABCDEF1234567890
+#   # ["arn:aws:iam::123456789012:", "oidc.eks.us-east-1.amazonaws.com/id/ABCDEF1234567890"]
+#   # element[1] means the 2nd part of the variable
+#   oidc_provider_host = element(
+#     split("oidc-provider/", var.oidc_provider_arn),
+#     1
+#   )
 
-  sa_subject = "system:serviceaccount:${var.namespace}:${var.service_account_name}"
-}
+#   sa_subject = "system:serviceaccount:${var.namespace}:${var.service_account_name}"
+# }
 
 resource "aws_iam_role" "this" {
   name = "${var.service_account_name}-irsa-role"
@@ -25,10 +25,11 @@ resource "aws_iam_role" "this" {
           Federated = var.oidc_provider_arn
         },
         Condition = {
-          StringEquals = {
-            "${local.oidc_provider_host}:sub" = local.sa_subject
-          }
+        StringEquals = {
+          "${replace(var.oidc_provider_url, "https://", "")}:sub" = "system:serviceaccount:${var.namespace}:${var.service_account_name}",
+          "${replace(var.oidc_provider_url, "https://", "")}:aud" = "sts.amazonaws.com"
         }
+      }
       }
     ]
   })
