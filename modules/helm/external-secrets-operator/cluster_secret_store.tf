fb3911acc83e9730f89512ba984ac2d8d7937b10
Adam Levi <adamlevi87@gmail.com>
2025-07-30 01:42:38 +0300

    patched

--- modules/helm/external-secrets-operator/cluster_secret_store.tf
+++ modules/helm/external-secrets-operator/cluster_secret_store.tf
@@ -1,39 +1,39 @@
-# # modules/external-secrets-operator/cluster_secret_store.tf
+# modules/external-secrets-operator/cluster_secret_store.tf
 
-# data "kubernetes_namespace" "kube_system" {
-#   metadata {
-#     name = "kube-system"
-#   }
-# }
+data "kubernetes_namespace" "kube_system" {
+  metadata {
+    name = var.namespace
+  }
+}
 
-# resource "kubernetes_manifest" "eso_cluster_secret_store" {
-#   manifest = {
-#     apiVersion = "external-secrets.io/v1beta1"
-#     kind       = "ClusterSecretStore"
-#     metadata = {
-#       name = "aws-secretsmanager"
-#     }
-#     spec = {
-#       provider = {
-#         aws = {
-#           service = "SecretsManager"
-#           region  = var.aws_region
+resource "kubernetes_manifest" "eso_cluster_secret_store" {
+  manifest = {
+    apiVersion = "external-secrets.io/v1beta1"
+    kind       = "ClusterSecretStore"
+    metadata = {
+      name = "aws-secretsmanager"
+    }
+    spec = {
+      provider = {
+        aws = {
+          service = "SecretsManager"
+          region  = var.aws_region
 
-#           auth = {
-#             jwt = {
-#               serviceAccountRef = {
-#                 name      = var.service_account_name
-#                 namespace = var.namespace
-#               }
-#             }
-#           }
-#         }
-#       }
-#     }
-#   }
+          auth = {
+            jwt = {
+              serviceAccountRef = {
+                name      = var.service_account_name
+                namespace = var.namespace
+              }
+            }
+          }
+        }
+      }
+    }
+  }
 
-#   depends_on = [
-#     helm_release.this,
-#     data.kubernetes_namespace.kube_system  # This ensures K8s connectivity
-#   ]
-# }
+  depends_on = [
+    helm_release.this,
+    data.kubernetes_namespace.kube_system  # This ensures K8s connectivity
+  ]
+}
