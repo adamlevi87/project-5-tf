23ccb4aeff55fe80ea9c9ebfa18c23afb08ce418
Adam Levi <adamlevi87@gmail.com>
2025-08-03 17:19:48 +0300

    commented an output in eks

--- modules/eks/outputs.tf
+++ modules/eks/outputs.tf
@@ -30,10 +30,10 @@ output "cluster_security_group_id" {
   value       = aws_eks_cluster.main.vpc_config[0].cluster_security_group_id
 }
 
-output "node_group_default_security_group" {
-  description = "Default Security group ID attached to the node group"
-  value       = data.aws_security_group.node_group_sg.id
-}
+# output "node_group_default_security_group" {
+#   description = "Default Security group ID attached to the node group"
+#   value       = data.aws_security_group.node_group_sg.id
+# }
 
 output "node_group_security_group_id" {
   description = "Security group ID attached to the node group"
