65865504ab2afdd7ed153e7b5c68b8e6ef39bc86
Adam Levi <adamlevi87@gmail.com>
2025-08-26 14:30:45 +0300

    metrics server fixed set usage

--- modules/helm/metrics-server/main.tf
+++ modules/helm/metrics-server/main.tf
@@ -10,58 +10,53 @@ resource "helm_release" "this" {
   namespace        = var.namespace
   create_namespace = false
 
-  # EKS-specific configuration
-  set {
-    name  = "args"
-    value = "{--cert-dir=/tmp,--secure-port=4443,--kubelet-preferred-address-types=InternalIP\\,ExternalIP\\,Hostname,--kubelet-use-node-status-port,--metric-resolution=15s,--kubelet-insecure-tls}"
-  }
-
-  set {
-    name  = "metrics.enabled"
-    value = "false"
-  }
-
-  set {
-    name  = "serviceMonitor.enabled"
-    value = "false"
-  }
-
-  # Resource configuration
-  set {
-    name  = "resources.requests.cpu"
-    value = var.cpu_requests
-  }
-
-  set {
-    name  = "resources.requests.memory"
-    value = var.memory_requests
-  }
-
-  set {
-    name  = "resources.limits.cpu"
-    value = var.cpu_limits
-  }
-
-  set {
-    name  = "resources.limits.memory"
-    value = var.memory_limits
-  }
-
-  # Node affinity for system workloads
-  set {
-    name  = "affinity.nodeAffinity.requiredDuringSchedulingIgnoredDuringExecution.nodeSelectorTerms[0].matchExpressions[0].key"
-    value = "kubernetes.io/os"
-  }
-
-  set {
-    name  = "affinity.nodeAffinity.requiredDuringSchedulingIgnoredDuringExecution.nodeSelectorTerms[0].matchExpressions[0].operator"
-    value = "In"
-  }
-
-  set {
-    name  = "affinity.nodeAffinity.requiredDuringSchedulingIgnoredDuringExecution.nodeSelectorTerms[0].matchExpressions[0].values[0]"
-    value = "linux"
-  }
+  # Configuration for metrics-server
+  set = [
+    # EKS-specific args
+    {
+      name  = "args"
+      value = "{--cert-dir=/tmp,--secure-port=4443,--kubelet-preferred-address-types=InternalIP\\,ExternalIP\\,Hostname,--kubelet-use-node-status-port,--metric-resolution=15s,--kubelet-insecure-tls}"
+    },
+    # Disable Prometheus integration (not needed for HPA)
+    {
+      name  = "metrics.enabled"
+      value = "false"
+    },
+    {
+      name  = "serviceMonitor.enabled"
+      value = "false"
+    },
+    # Resource configuration
+    {
+      name  = "resources.requests.cpu"
+      value = var.cpu_requests
+    },
+    {
+      name  = "resources.requests.memory"
+      value = var.memory_requests
+    },
+    {
+      name  = "resources.limits.cpu"
+      value = var.cpu_limits
+    },
+    {
+      name  = "resources.limits.memory"
+      value = var.memory_limits
+    },
+    # Node affinity for system workloads (Linux nodes only)
+    {
+      name  = "affinity.nodeAffinity.requiredDuringSchedulingIgnoredDuringExecution.nodeSelectorTerms[0].matchExpressions[0].key"
+      value = "kubernetes.io/os"
+    },
+    {
+      name  = "affinity.nodeAffinity.requiredDuringSchedulingIgnoredDuringExecution.nodeSelectorTerms[0].matchExpressions[0].operator"
+      value = "In"
+    },
+    {
+      name  = "affinity.nodeAffinity.requiredDuringSchedulingIgnoredDuringExecution.nodeSelectorTerms[0].matchExpressions[0].values[0]"
+      value = "linux"
+    }
+  ]
 
   depends_on = [var.eks_dependency]
 
