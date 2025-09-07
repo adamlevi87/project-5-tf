0529b561cf44af8d664d6c4f00cf0033102cda76
Adam Levi <adamlevi87@gmail.com>
2025-09-07 21:37:45 +0300

    added changes lambda

--- main/lambda-code/index.js
+++ main/lambda-code/index.js
@@ -1,5 +1,5 @@
 // lambda-code/index.js
-import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
+import { S3Client, PutObjectCommand, ListObjectsV2Command } from '@aws-sdk/client-s3';
 import { randomUUID } from 'crypto';
 
 // Initialize AWS S3 client
