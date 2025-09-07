e72e067e10fe576d5cfe77ecd267c369b79fa0da
Adam Levi <adamlevi87@gmail.com>
2025-09-07 19:17:11 +0300

    kms module updated key set up

--- modules/kms/main.tf
+++ modules/kms/main.tf
@@ -6,6 +6,34 @@ resource "aws_kms_key" "s3_key" {
   deletion_window_in_days = var.deletion_window_in_days
   enable_key_rotation     = var.enable_key_rotation
 
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "Enable IAM policies"
+        Effect = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::${var.account_id}:root"
+        }
+        Action = "kms:*"
+        Resource = "*"
+      },
+      # Temporary permissions for cloudfront(s) to use the key
+      {
+        Sid    = "Allow CloudFront to use the key"
+        Effect = "Allow"
+        Principal = {
+          Service = "cloudfront.amazonaws.com"
+        }
+        Action = [
+          "kms:Decrypt",
+          "kms:GenerateDataKey*"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+
   tags = {
     Name        = "${var.project_tag}-${var.environment}-s3-kms-key"
     Project     = var.project_tag
@@ -24,6 +52,8 @@ resource "aws_kms_alias" "s3_key_alias" {
 resource "aws_iam_role" "kms_key_role" {
   name = "${var.project_tag}-${var.environment}-kms-key-role"
 
+  # Allowing KMS to work with IAM policies
+  # Additional per user/Role permissions are needed and handled separately
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
@@ -81,43 +111,8 @@ resource "aws_iam_policy" "kms_key_admin_policy" {
   }
 }
 
-# IAM policy for CloudFront service to use the key (global service, so kept here)
-resource "aws_iam_policy" "kms_cloudfront_policy" {
-  name        = "${var.project_tag}-${var.environment}-kms-cloudfront-access"
-  description = "IAM policy for CloudFront service to use KMS key"
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Principal = {
-          Service = "cloudfront.amazonaws.com"
-        }
-        Action = [
-          "kms:Decrypt",
-          "kms:GenerateDataKey*"
-        ]
-        Resource = aws_kms_key.s3_key.arn
-      }
-    ]
-  })
-
-  tags = {
-    Name        = "${var.project_tag}-${var.environment}-kms-cloudfront-policy"
-    Project     = var.project_tag
-    Environment = var.environment
-  }
-}
-
 # Attach admin policy to role
 resource "aws_iam_role_policy_attachment" "kms_admin_attachment" {
   role       = aws_iam_role.kms_key_role.name
   policy_arn = aws_iam_policy.kms_key_admin_policy.arn
-}
-
-# Attach CloudFront policy to role
-resource "aws_iam_role_policy_attachment" "kms_cloudfront_attachment" {
-  role       = aws_iam_role.kms_key_role.name
-  policy_arn = aws_iam_policy.kms_cloudfront_policy.arn
 }
